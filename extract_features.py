ff4ee5fb016526c71810fd94cf48d28622b62084
Vassili Korotkin <vkorotkin@gmail.com>
2019-02-11 14:46:29 -0500

    abstraction

--- extract_features.py
+++ extract_features.py
@@ -19,60 +19,58 @@ from sklearn.feature_extraction.text import TfidfTransformer
 
 
 class MyTokenizer(object):
-    def __init__(self, Stemmer):
-        """Give this either 
-        WordNetLemmatizer()
-        or nltk.PorterStemmer()"""
+    def __init__(self):
+        """Copy pasted from the sklearn feature extraction documentation
+        https://scikit-learn.org/stable/modules/feature_extraction.html"""
         self.wnl = WordNetLemmatizer()
     def __call__(self, doc):
         return [self.wnl.lemmatize(t) for t in word_tokenize(doc)]
-    
 
-tokenizer=MyTokenizer(WordNetLemmatizer())
-lemma_vect = CountVectorizer(tokenizer=MyTokenizer(WordNetLemmatizer()))
-
-num_files_to_read_per_sent=4000;
-
-def load_raw_data():
+def load_raw_data(num_files_to_read_per_sent):
+    """Load raw files into python lists. Assumes each file is in e.g. data/train/pos
+    corpus: list of strings, one for each file
+    reviews: vector, 1 if corresponding corpus is pos, 0 if neg.  """
     corpus=[]
     reviews=[]
     for foldername in ['pos', 'neg']:
         file_list=os.listdir('data/train/'+ foldername)
         for fname in file_list[0:num_files_to_read_per_sent]:
             path='data/train/'+foldername+'/'+fname
-            #f=open('data/train/pos/0_9.txt')
             f=open(path)
             raw=f.read()
             corpus.append(raw)
             reviews.append(1 if foldername=='pos' else 0)
     return corpus, reviews
 
-corpus, reviews=load_raw_data()
 
-X_train, X_val, y_train, y_val = train_test_split(corpus, reviews, train_size=0.8, test_size=0.2)
+def construct_data_matrices():
+    """Get back data matrices for the pos/neg examples. """
+    corpus, reviews=load_raw_data(num_files_to_read_per_sent=4000) #HOW MANY FILES TO LOAD
+    X_train, X_val, y_train, y_val = train_test_split(corpus, reviews, train_size=0.8, test_size=0.2)
+    
+    #build the corpus->matrix converter
+    lemma_vect = CountVectorizer(tokenizer=MyTokenizer()) #Use a lemmatizer, add to the countvectorizer
+    
+    #count only
+    count_vect = lemma_vect.fit(X_train)
+    X_train_counts = count_vect.transform(X_train)
+    X_val_counts = count_vect.transform(X_val) 
+    
+    #tf-idf only
+    tfidf_transformer = TfidfTransformer().fit(X_train_counts)
+    X_train_tfidf = tfidf_transformer.transform(X_train_counts)
+    X_val_tfidf = tfidf_transformer.transform(X_val_counts)
+    
+    #Both together
+    X_train_mat=hstack([X_train_counts, X_train_tfidf]).toarray()
+    X_val_mat=hstack([X_val_counts, X_val_tfidf]).toarray()
+
+    return X_train_mat, X_val_mat
 
 
-count_vect = lemma_vect.fit(X_train)
-X_train_counts = count_vect.transform(X_train)
-X_val_counts = count_vect.transform(X_val) 
-
-tfidf_transformer = TfidfTransformer().fit(X_train_counts)
-X_train_tfidf = tfidf_transformer.transform(X_train_counts)
-X_val_tfidf = tfidf_transformer.transform(X_val_counts)
-
-
-X_train=hstack([X_train_counts, X_train_tfidf]).toarray()
-X_val=hstack([X_val_counts, X_val_tfidf]).toarray()
 
 
 
-#    tokens=word_tokenize(raw)
-#tokens=[porter.stem(t) for t in tokens]
-
-#tokens=word_tokenize(raw)
-#tokens=[porter.stem(t) for t in tokens]
-#all_words=all_words+tokens
-#documents.append((tokens, foldername))
 
 
 
