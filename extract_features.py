79018302f777a8979c25e3f54c3bee89472c4cb5
Vassili Korotkin <vkorotkin@gmail.com>
2019-02-14 16:43:50 -0500

    Minimal working example

--- extract_features.py
+++ extract_features.py
@@ -7,16 +7,14 @@ Created on Mon Feb 11 11:03:22 2019
 """
 
 import os
-import nltk
+import numpy as np
 from nltk import word_tokenize          
 from nltk.stem import WordNetLemmatizer 
-
 from scipy.sparse import hstack
+
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
-
-
 
 class MyTokenizer(object):
     def __init__(self):
@@ -31,7 +29,8 @@ def load_raw_data(num_files_to_read_per_sent):
     corpus: list of strings, one for each file
     reviews: vector, 1 if corresponding corpus is pos, 0 if neg.  """
     corpus=[]
-    reviews=[]
+    reviews=-1*np.ones([num_files_to_read_per_sent*2])
+    i=0;
     for foldername in ['pos', 'neg']:
         file_list=os.listdir('data/train/'+ foldername)
         for fname in file_list[0:num_files_to_read_per_sent]:
@@ -39,24 +38,21 @@ def load_raw_data(num_files_to_read_per_sent):
             f=open(path)
             raw=f.read()
             corpus.append(raw)
-            reviews.append(1 if foldername=='pos' else 0)
+            reviews[i]=(1 if foldername=='pos' else 0)
+            i=i+1;
     return corpus, reviews
 
-
 def construct_data_matrices():
-    """Get back data matrices for the pos/neg examples. """
+    """Was initial version, here only for demosntraion/reference"""
     corpus, reviews=load_raw_data(num_files_to_read_per_sent=4000) #HOW MANY FILES TO LOAD
     X_train, X_val, y_train, y_val = train_test_split(corpus, reviews, train_size=0.8, test_size=0.2)
     
-    #build the corpus->matrix converter
     lemma_vect = CountVectorizer(tokenizer=MyTokenizer()) #Use a lemmatizer, add to the countvectorizer
-    
-    #count only
     count_vect = lemma_vect.fit(X_train)
     X_train_counts = count_vect.transform(X_train)
     X_val_counts = count_vect.transform(X_val) 
     
-    #tf-idf only
+#tf-idf only
     tfidf_transformer = TfidfTransformer().fit(X_train_counts)
     X_train_tfidf = tfidf_transformer.transform(X_train_counts)
     X_val_tfidf = tfidf_transformer.transform(X_val_counts)
@@ -64,7 +60,6 @@ def construct_data_matrices():
     #Both together
     X_train_mat=hstack([X_train_counts, X_train_tfidf]).toarray()
     X_val_mat=hstack([X_val_counts, X_val_tfidf]).toarray()
-
     return X_train_mat, X_val_mat
 
 
--- main.py
+++ main.py
@@ -6,10 +6,36 @@ Created on Mon Feb 11 14:22:54 2019
 @author: vassili
 """
 
-from extract_features import construct_data_matrices
+from extract_features import MyTokenizer, load_raw_data
 
-# Note that we can try to do this with the Pipeline Makeover (see SKLearn tutorial colab)
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import Normalizer
 
-#For now, can modify the tfidf, etc. stuff in extract_features, but really should do pipeline 
-#together with the fitting. 
-X_train_mat, X_val_mat = construct_data_matrices()
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+
+
+
+"""Get back data matrices for the pos/neg examples. """
+corpus, reviews=load_raw_data(num_files_to_read_per_sent=4000) #HOW MANY FILES TO LOAD
+
+X_train, X_val, y_train, y_val = train_test_split(corpus, reviews, train_size=0.8, test_size=0.2)
+
+#build the corpus->matrix converter
+lemma_vect = CountVectorizer(tokenizer=MyTokenizer()) #Use a lemmatizer, add to the countvectorizer
+
+pclf = Pipeline([
+    ('vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer()),
+    ('norm', Normalizer()),
+    ('clf', MultinomialNB()),
+])
+
+pclf.fit(X_train, y_train)
+y_pred = pclf.predict(X_val)
+print(metrics.classification_report(y_val, y_pred,
+    target_names=["Review"]))
