bfecd423392b8831756b9d78137fa42a53d7e0bf
Vassili Korotkin <vkorotkin@gmail.com>
2019-02-14 17:06:15 -0500

    Implement loop to go through different classifiers

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Created on Mon Feb 11 14:22:54 2019
 """
 
 from extract_features import MyTokenizer, load_raw_data
-
+import numpy as np
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
@@ -25,11 +25,14 @@ corpus, reviews=load_raw_data(num_files_to_read_per_sent=4000) #HOW MANY FILES T
 
 X_train, X_val, y_train, y_val = train_test_split(corpus, reviews, train_size=0.8, test_size=0.2)
 
-#build the corpus->matrix converter
+#This can take the place of CountVectorizer below - it lemmatizes.
+#comparisons could be interesting
 lemma_vect = CountVectorizer(tokenizer=MyTokenizer()) #Use a lemmatizer, add to the countvectorizer
 
+
+#This is a problem..Run this, bottom for loop detects two distinct class labels, here only one. Why?
 pclf = Pipeline([
-    ('vect', CountVectorizer()),
+    ('vect', lemma_vect),
     ('tfidf', TfidfTransformer()),
     ('norm', Normalizer()),
     ('clf', MultinomialNB()),
@@ -37,5 +40,63 @@ pclf = Pipeline([
 
 pclf.fit(X_train, y_train)
 y_pred = pclf.predict(X_val)
+
 print(metrics.classification_report(y_val, y_pred,
     target_names=["Review"]))
+
+#Compare classifiers on this dataset with this lemmatization, 
+#Very similar to what is done at the scikit learn link below
+#https://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html
+names=["MultinomialNB", "SVM"]
+classifiers=[
+        MultinomialNB(),
+        SVC()
+        ]
+
+rng = np.random.RandomState(2)
+
+for name, clf in zip(names, classifiers):
+    pclf = Pipeline([
+    ('vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer()),
+    ('norm', Normalizer()),
+    ('clf', clf),
+    ])
+    pclf.fit(X_train, y_train)
+    y_pred = pclf.predict(X_val)
+    score = pclf.score(X_val, y_val)
+    print("Model results for {}".format(name))
+    print("Accuracy:{}".format(score))
+    print(metrics.classification_report(y_val, y_pred,
+    target_names=["Positive", "Negative"])) # not sure about this, these should be the class labels
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
